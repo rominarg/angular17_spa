d964173ed4d23140a08315bd88f91c95f411745d
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-01 07:20:45 -0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { IniciarSesionComponent } from './pages/auth/iniciar-sesion/iniciar-sesion.component';
+import { RegistroComponent } from './pages/auth/registro/registro.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component:HomeComponent},
+    {path:"panel-control", component:DashboardComponent}, 
+    {path:"iniciar-sesion", component:IniciarSesionComponent},
+    {path:"registro", component:RegistroComponent},
+    {path:"quienes-somos", component:QuienesSomosComponent},
+    {path:"servicios", component:ServiciosComponent},
+];
