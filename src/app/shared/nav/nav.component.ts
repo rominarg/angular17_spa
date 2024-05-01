93d03b10bc0febd3b8314e9f1b04263697623fc8
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-01 07:50:15 -0300

    navcomponent

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
+  estaAutenticado=false;
 }
