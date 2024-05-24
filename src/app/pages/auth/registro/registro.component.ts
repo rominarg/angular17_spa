9e0c1e0259cb483af46adc131cacca86d3e20e6c
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-24 12:33:31 -0300

    modelos y servicios

--- src/app/pages/auth/registro/registro.component.ts
+++ src/app/pages/auth/registro/registro.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { User } from '../../../models/user';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {  url="https://reqres.in/api/users/1";
+
+
+  constructor(private http:HttpClient) { }
+
+  createUser(user:User):Observable<any>
+  {
+    return this.http.post(this.url, user)
+  }
+}
+
+
+
+
+
