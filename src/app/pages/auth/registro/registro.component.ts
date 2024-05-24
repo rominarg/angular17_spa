2cdd17e61a8deb26810922fe443e1bc58b13b71a
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-24 13:49:40 -0300

    modelos y servicios

--- src/app/pages/auth/registro/registro.component.ts
+++ src/app/pages/auth/registro/registro.component.ts
@@ -12,5 +12,44 @@ import { Router } from '@angular/router';
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  form:FormGroup;
+  usuario: User = new User();
+
+  constructor(private formBuilder: FormBuilder, private authService:AuthService , private router: Router) { 
+    this.form= this.formBuilder.group(
+      {
+        nombre: ['', [Validators.required, Validators.minLength(3)]], // Mínimo 3 caracteres
+        apellido: ['', [Validators.required]],
+        dni: ['', [Validators.required, Validators.pattern('[0-9]{8}')]], 
+        fechaNacimiento: ['', [Validators.required]],
+        password1: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]], // Mínimo 8 caracteres, al menos una mayúscula, una minúscula, un número y un carácter especial
+        password2:['',[Validators.required]],
+        email:['', [Validators.required, Validators.email]]   
+      }
+    )
+  }
+
+  onEnviar(event: Event): void {
+    event.preventDefault; 
+    if (this.form.valid)
+    {
+      console.log("Enviando  al servidor...");
+      
+      this.authService.createUser(this.form.value as User).subscribe(
+        data => {
+          console.log(data.id);
+          console.log( this.form.value as User)
+            if (data.id>0)
+            {
+              alert("El registro ha sido creado satisfactoriamente. A continuación, por favor Inicie Sesión.");
+              this.router.navigate(['/iniciar-sesion'])
+            }
+        })
+    }
+    else
+    {
+      this.form.markAllAsTouched(); 
+    }
+  }
 
 }
