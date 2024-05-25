118e6275ea72f6763b94f287a43e14dc64766824
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-25 06:11:08 -0300

    corrigo errores y agrego el db.json

--- src/app/pages/quienes-somos/quienes-somos.component.ts
+++ src/app/pages/quienes-somos/quienes-somos.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+// import { RouterLink } from '@angular/router';
+import { QuienesSomosService } from '../../services/quienes-somos.service';
 
 @Component({
   selector: 'app-quienes-somos',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './quienes-somos.component.html',
   styleUrl: './quienes-somos.component.css'
 })
 export class QuienesSomosComponent {
-
+  profesionalList:any;
+  constructor(private QuienesSomosService:QuienesSomosService)
+  {
+    this.profesionalList=QuienesSomosService.obtenerProfesionales().subscribe({
+      next: (profesionalList) => {
+        this.profesionalList=profesionalList;
+      },
+      error: (error) => {
+        console.error(error)
+      }
+    });
+  }
 }
